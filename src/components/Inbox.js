0f67b6c276d06088d6f3ec46b48fe0a25fb86ea3
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-09 21:24:24 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,55 +1,7 @@
-// import React, { useState, useRef } from "react";
-
-// const Inbox = (props) => {
- 
-
-//   return (
-//     <div>
-//       <h3>Inbox</h3>
-//       {!newTask && (
-//         <button className="new" onClick={} id='add-new'>
-//           +Add New
-//         </button>
-//       )}
-//       {newTask && (
-//         <form className="newtask-box">
-//           <input type="text" id="title" ref={}></input>
-//           <div className="buttons">
-//             <button className="new" id="add-list" onClick={}>
-//               Add Task
-//             </button>
-//             <button className="new" onClick={}>
-//               Cancel
-//             </button>
-//             <input
-//               type="date"
-//               ref={}
-//               defaultValue="2022-09-27"
-//               id="date"
-//             ></input>
-//           </div>
-//         </form>
-//       )}
-//       <div id="inbox">
-//         {props.list.map((list) => {
-//           return (
-//             <div className="box" key={}>
-//               <div className="task">
-//                 {} ({})
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Inbox;
 import React, { useState, useRef } from "react";
 
 const Inbox = (props) => {
- const [newTask, setNewTask] = useState(false);
+  const [newTask, setNewTask] = useState(false);
 
   const titleRef = useRef(null);
   const calendarRef = useRef(null);
@@ -76,13 +28,12 @@ const Inbox = (props) => {
   const cancelHandler = (e) => {
     setNewTask(false);
   };
- 
 
   return (
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={newTaskHandler} id='add-new'>
+        <button className="new" onClick={newTaskHandler} id="add-new">
           +Add New
         </button>
       )}
@@ -108,7 +59,7 @@ const Inbox = (props) => {
       <div id="inbox">
         {props.list.map((list) => {
           return (
-            <div className="box">
+            <div className="box" key={list.number}>
               <div className="task">
                 {list.title} ({list.date.toLocaleDateString()})
               </div>
